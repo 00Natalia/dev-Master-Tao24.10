b9258598cee369eceb441a8119592e49bfdebff0
00Natalia <potapovanatalia434@gmail.com>
2024-03-08 15:54:01 +0200

    feat: burder menu script done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,9 @@
-  
+const burger = document.querySelector('.btm-burger');
+const navigation = document.querySelector('.header__navigation'); 
+
+const toggleMenu =()=>{
+    navigation.classList.toggle('show');
+    burger.classList.toggle('open');
+}
+
+burger.addEventListener('click', toggleMenu)
